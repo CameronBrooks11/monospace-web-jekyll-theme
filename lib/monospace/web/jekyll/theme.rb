6a1177f15d920b8e65914e496d00d7fc382e368d
Cameron K Brooks <cambrooks3393@gmail.com>
2025-06-07 16:43:06 -0400

    Initialize Jekyll theme structure

---
+++ lib/monospace/web/jekyll/theme.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative "theme/version"
+
+module Monospace
+  module Web
+    module Jekyll
+      module Theme
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/monospace/web/jekyll/theme/version.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Monospace
+  module Web
+    module Jekyll
+      module Theme
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
